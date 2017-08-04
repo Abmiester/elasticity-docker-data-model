994fdb4c9c50f3e7432560c42031baa5088f4226
valiantone <zrjohn@yahoo.com>
2017-08-04 16:27:08 -0400

    Connection timeout error still needs to be fixed

--- src/run.py
+++ src/run.py
@@ -1,41 +1,32 @@
-# Main python file for model code
-# 
-# Default Environment Variables
-#
-# GOSPEL_DB_URL - The database url string for connecting to gospel
-# DOCKER_ML_OUTPUT_SCHEMA - Standard schema to use for model results/metrics
-#
-# How To Add Additional environment variables
-# 
-# In the Airflow variables UI ($AIRFLOW_URL/admin/variable/) edit the
-# DOCKERIZED_ML_TASKS variable, find the entry in the JSON list corresponding
-# to your model, add a key "env" to the JSON object with a key matching your environment
-# variable name. Ex.
-#
-# [
-# ...
-# {
-#   "name": "demand-projections-high",
-#    "git_repo": "git@github.com:/bellhops/demand-projections-1",
-#      "env": {
-#        "output_table": "demand_projections_low",
-#        "projection_strategy": "low"
-#      }
-# },
-# ...
-# ]
-#
-# You can then access this model using this python snipet:
-# CONSTANT = json.loads(os.environ.get('CONSTANT', <default>))
-#
-
-import os
-import json
+from os import environ
+import pandas
 from sqlalchemy import create_engine
+from pandas import DataFrame
+
+LB = environ['LOWER_PRICE_BOUND'] if 'LOWER_PRICE_BOUND' in environ else 40
+UB = environ['UPPER_PRICE_BOUND'] if 'UPPER_PRICE_BOUND' in environ else 60
+INC = environ['INCREMENTS'] if 'INCREMENTS' in environ else 62
+
+def daily_market_metrics(destination_engine):
+    with destination_engine.connect() as connection:
+        visitors = connection.execute("SELECT date_trunc('day', sent_at at time zone 'UTC') as date, COUNT(DISTINCT anonymous_id) as visitors FROM web_prod.pages GROUP BY date_trunc('day', sent_at at time zone 'UTC')")
+        bookings = connection.execute("SELECT date_trunc('day', booked_at at time zone 'UTC') as date, COUNT(DISTINCT id) as bookings FROM kinetic.reservations_order WHERE order_status!='pending' GROUP BY date_trunc('day', booked_at at time zone 'UTC')")
+
+    conv = DataFrame.from_records(visitors.fetchall(), columns=['date', 'visitors']).set_index("date")
+    rf = DataFrame.from_records(bookings.fetchall(), columns=['date', 'bookings']).set_index("date")
+    conv['bookings'] = rf['bookings']
+    conv['rate'] = conv.bookings/conv.visitors
+    rate_at_price = {40: conv[:'2017-06-14'].tail(len(conv['2017-06-15':])).rate.mean(), 45: conv['2017-06-15':].rate.mean()}
+    slope = ((rate_at_price[45] - rate_at_price[40]))/float(5)
+    result = conv['2017-06-16':]
+
+    for i in range(LB, UB+1, INC):
+        result['rate_at_' + str(i)] = result['rate'] + slope * (i-LB)
+
+    with destination_engine.connect() as connection:
+        result.drop('rate', axis=1).tail(1).to_sql(con=connection, name='daily_global_test', if_exists='append', schema='forecast')
+
 
 if __name__ == "__main__":
-    gospel_conn = create_engine(os.environ['GOSPEL_DB_URL'])
-    
-    # Model code that needs to be executed goes here, what this "main" portion of the model is doing
-    # should be as simple/intuitive as possible use functions/libraries to encapsulte other code
-    print("Hello world")
+    gospel_engine = create_engine(environ['GOSPEL_DB_URL'])
+    daily_market_metrics(gospel_engine)
